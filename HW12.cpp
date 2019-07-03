5ac0d2e55bcbf8ed5d361ec66721aabff3a03d4e
gatsukichi <52203107+gatsukichi@users.noreply.github.com>
2019-07-03 23:43:10 +0900

    HW12

---
+++ HW12.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char name[20];
+
+	printf("이름을 입력하시오 : ");
+	fgets(name, sizeof(name), stdin);
+	
+	name[strlen(name) - 1] = '\0';
+
+	printf("\" %s \"\n", name);
+	printf("\" %20s \"\n", name);
+	printf("\"%-20s\"\n", name);
+
+
+	return 0;
+}
+
+
